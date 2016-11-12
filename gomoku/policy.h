d26f554512ecd4caed1b9478c810e2940b987f17
williamd4112 <williamd4112@hotmail.com>
2016-11-12 18:43:43 +0800

    Modify policy.h, see policy_aggresvie

--- gomoku/policy.h
+++ gomoku/policy.h
@@ -93,7 +93,7 @@ namespace mcts {
         }
       }
     }
-
+    std::random_shuffle(next_states.begin(), next_states.end());
     return next_states.empty() ? POLICY_FAIL : POLICY_SUCCESS;
   }
 
@@ -101,127 +101,80 @@ namespace mcts {
   {
     /// TODO : implement approach policy
     DEBUG_POLICY("Policy approach (%d)\n", state.agent_id);
-    int result = 0;
+    int result = POLICY_FAIL;
     std::vector<Tss::threat_t> approaches;
     Tss approach_tss(state);
     approach_tss.find_all_threats(state.position, approaches, THREAT_LEVEL_2, THREAT_LEVEL_2, 1);
 
     if (!approaches.empty()) {
       expand_threats_to_states(approaches, state, next_states);
-      result = 1;
+      result = POLICY_SUCCESS;
     }
 
-    return result;
-  }
-
-  int policy_defend_winning_seq(const State & state, const std::vector<Tss::threat_t> & threats, std::vector<State> & next_states)
-  {
-    std::vector<Tss::threat_t> winning_seq;
-    find_winning_sequence(threats, winning_seq);
-
-    if (winning_seq.empty()) {
-      DEBUG_POLICY("No winning sequence\n");
-      expand_threats_to_states(threats, state, next_states);
-    }
-    else {
-      DEBUG_POLICY("Winning sequence detect\n");
-      expand_threats_to_states(winning_seq, state, next_states);
-    }
-
-    return POLICY_SUCCESS;
-  }
-
-  int policy_defend_urgent_move(const State & state, const std::vector<State> & next_states)
-  {
-    int result = POLICY_FAIL;
-
-    return result;
-  }
-
-  int policy_defend(const State & state, const std::vector<Tss::threat_t> & threats, std::vector<State> & next_states)
-  {
-    int result = 0;
-    int one_step_win_threat_index = find_one_step_winning(threats);
-    if (one_step_win_threat_index < 0) {
-      result = policy_defend_winning_seq(state, threats, next_states);
-    }
-    else {
-      expand_threat_to_states(threats[one_step_win_threat_index], state, next_states);
-      result = ONE_STEP_WIN;
-    }
     return result;
   }
 
   int policy_attack(const State & state, const std::vector<Tss::threat_t> & threats, std::vector<State> & next_states)
   {
-    int result = 0;
+    int result = POLICY_FAIL;
     if (!threats.empty()) {
-      result = policy_defend_winning_seq(state, threats, next_states);
+        expand_threats_to_states(threats, state, next_states);
+        result = POLICY_SUCCESS;
     }
     else {
-      /// TODO : Implement policy when no threats
       DEBUG_POLICY("No threats, approach\n");
-      if (policy_attack_approach(state, next_states) != POLICY_SUCCESS) {
-        if (policy_attack_random(state, next_states) != POLICY_SUCCESS) {
-          policy_attack_middle(state, next_states);
+      if ((result = policy_attack_approach(state, next_states) != POLICY_SUCCESS)) {
+        if ((result = policy_attack_random(state, next_states) != POLICY_SUCCESS)) {
+          result = policy_attack_middle(state, next_states);
         }
       }
     }
     return result;
   }
 
-  int policy_default(const State & opponent_state, std::vector<State> & next_states)
+  int policy_aggresive(const State & opponent_state, std::vector<State> & next_states)
   {
-    int result = 0;
+    int res = POLICY_FAIL;
+
+    State self_state(opponent_state);
+    self_state.agent_id ^= (1 << 0);
 
     /* Search opponent winning sequence */
     std::vector<Tss::threat_t> opponent_threats;
+    std::vector<Tss::threat_t> opponent_winning_seq;
     Tss opponent_tss(opponent_state);
     opponent_tss.find_all_threats(opponent_state.position, opponent_threats, THREAT_LEVEL_3, THREAT_LEVEL_5, OPPONENT_TSS_MAX_DEPTH);
     std::sort(opponent_threats.begin(), opponent_threats.end(), std::greater<Tss::threat_t>());
-    DEBUG_POLICY("Opponent threats : %d\n", opponent_threats.size());
-    std::cout << opponent_threats << std::endl;
-    for (auto t : opponent_threats)
-        t.print();
+    DEBUG_POLICY("Opponent winning seq\n");
+    find_winning_sequence(opponent_threats, opponent_winning_seq);
 
-    /* Search self winning sequence */
-    State self_state(opponent_state);
-    self_state.agent_id = opponent_state.agent_id ^ (1 << 0);
-
+    /* Search self winning sequences */
     std::vector<Tss::threat_t> self_threats;
+    std::vector<Tss::threat_t> self_winning_seq;
     Tss self_tss(self_state);
     self_tss.find_all_threats(self_state.position, self_threats, THREAT_LEVEL_3, THREAT_LEVEL_5, DEFAULT_TSS_MAX_DEPTH);
     std::sort(self_threats.begin(), self_threats.end(), std::greater<Tss::threat_t>());
-    DEBUG_POLICY("Self threats : %d\n",self_threats.size());
-    std::cout << self_threats << std::endl;
+    DEBUG_POLICY("Self winning seq\n");
+    find_winning_sequence(self_threats, self_winning_seq);
 
-    DEBUG_POLICY("Opponent id = %d\n", opponent_state.agent_id);
-    DEBUG_POLICY("Self id = %d\n", self_state.agent_id);
-
-    /* Check self one step winning */
-    int one_step_win_threat_index = find_one_step_winning(self_threats);
-    if (one_step_win_threat_index < 0) {
-      /// TODO Merge opponent threats and self threats to find optimal move
-      DEBUG_POLICY("No-one step winning:\n");
-
-      /* Defend when opponent_threats is not empty */
-      if (!opponent_threats.empty()) {
-        DEBUG_POLICY("Policy defend\n");
-        /* Find threats using opponent state, expand using self_state */
-        result = policy_defend(self_state, opponent_threats, next_states);
-      }
-      /* Attack when opponent threats is empty, and no one step winning */
-      else {
-        DEBUG_POLICY("Policy attack\n");
-        result = policy_attack(self_state, self_threats, next_states);
-      }
+    /* Compare winning seq depth, attack/defend by winning seq */
+    int opponent_min_winning_depth = (opponent_winning_seq.empty()) ? INT_MAX : opponent_winning_seq.front().min_winning_depth;
+    int self_min_winning_depth = (self_winning_seq.empty()) ? INT_MAX : self_winning_seq.front().min_winning_depth;
+    if (self_min_winning_depth < opponent_min_winning_depth && !self_winning_seq.empty()) {
+        expand_threat_to_states(self_winning_seq.front(), self_state, next_states);
+        res = POLICY_SUCCESS;
     }
-    else {
-      DEBUG_POLICY("One step winning:\n");
-      expand_threat_to_states(self_threats[one_step_win_threat_index], self_state, next_states);
-      result = ONE_STEP_WIN;
+    else if (!opponent_winning_seq.empty()){
+        expand_threat_to_states(opponent_winning_seq.front(), self_state, next_states);
+        res = POLICY_SUCCESS;
     }
-    return result;
+
+    /* No emergent status, attack with self threats */
+    if (res != POLICY_SUCCESS) {
+        res = policy_attack(self_state, self_threats, next_states);
+    }
+
+    return res;
   }
 }
 
--- gomoku/test_policy.cpp
+++ gomoku/test_policy.cpp
@@ -2,26 +2,37 @@
 #include "policy.h"
 #include "state.h"
 #include "constants.h"
+#include <cstdlib>
+#include <cstdio>
 #include <iostream>
 #include <queue>
 
 using namespace std;
 using namespace mcts;
 
-void expand(const State & s, int depth)
+int check_win(const State & state)
 {
-  if (depth > 0) return;
+    return EMPTY;
+}
+
+void expand(const State & s, int depth, int max_depth)
+{
+  if (depth >= max_depth) return;
   cout << "Depth " << depth << " : Agent " << (int)s.agent_id << endl;
   cout << s << endl;
   std::vector<State> next_states;
-  policy_default(s, next_states);
+  policy_aggresive(s, next_states);
 
+#if 0
   for (State & next : next_states) {
-    expand(next, depth + 1);
+    expand(next, depth + 1, max_depth);
   }
+#else
+  expand(next_states[rand() % next_states.size()], depth + 1, max_depth);
+#endif
 }
 
-int main()
+int main(int argc, char * argv[])
 {
   State state(15, 15, WHITE);
   for (int i = 0; i < 15; i++) {
@@ -33,7 +44,7 @@ int main()
     }
     cout << endl;
   }
-  expand(state, 0);
+  expand(state, 0, atoi(argv[1]));
 
   return 0;
 }
